3fce6530c6260d73f707c5ea3cecce3214774d29
Fangzheng Liu <fzliu@mit.edu>
2023-06-18 17:55:54 -0400

    udated by FZ from Windows

---
+++ nRF52840_RTC_tst/src/RTC.h
@@ -0,0 +1,4 @@
+#include <Arduino.h>
+
+#define LFCLK_START_TASK (1UL)
+#define LFCLK_STOP_TASK  (1UL)
---
+++ update.sh
@@ -0,0 +1,5 @@
+git status
+git add --all
+git commit -a -m "udated by FZ from Windows"
+git pull origin main
+git push origin HEAD:main
